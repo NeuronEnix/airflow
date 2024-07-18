dd4571b767ec36ca60f7c07efddd018129659384
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-07-18 12:15:58 +0530

    init: with hello world dag, bash and python operator

---
+++ hello.py
@@ -0,0 +1,32 @@
+# hello_world.py
+
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+
+def print_hello():
+    return "Hello, World!"
+
+
+dag = DAG(
+    "hello_world",
+    description="Simple tutorial DAG",
+    schedule_interval="0 3 * * *",
+    start_date=datetime(2024, 6, 1),
+    catchup=True,
+)
+
+py_task = PythonOperator(
+    task_id="py_task", python_callable=print_hello, dag=dag
+)
+
+bash_task = BashOperator(
+    task_id="bash_task",
+    bash_command="echo 'Hello, Bash!'",
+    dag=dag,
+)
+
+py_task >> bash_task
