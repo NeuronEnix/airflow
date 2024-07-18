0eb92016c08464efc79aebc89f1cbb6c0701cd70
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-07-18 19:16:10 +0530

    added branches

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -1,9 +1,9 @@
 # dag.py
 
 from airflow import DAG
-
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 
 
@@ -11,9 +11,17 @@ def print_hello():
     return "Hello, World!"
 
 
+def choose_branch():
+    # Simple condition for branching
+    if datetime.now().minute % 2 == 0:
+        return "even_minute_task"
+    else:
+        return "odd_minute_task"
+
+
 dag = DAG(
     "hello_world",
-    description="Simple tutorial DAG",
+    description="Simple tutorial DAG with branching",
     schedule_interval="0 2 * * *",
     start_date=datetime(2024, 6, 1),
     catchup=True,
@@ -29,4 +37,36 @@ bash_task = BashOperator(
     dag=dag,
 )
 
-py_task >> bash_task
+branch_task = BranchPythonOperator(
+    task_id="branch_task",
+    python_callable=choose_branch,
+    dag=dag,
+)
+
+even_minute_task = BashOperator(
+    task_id="even_minute_task",
+    bash_command="echo 'even_minute_task'",
+    dag=dag,
+)
+
+odd_minute_task = BashOperator(
+    task_id="odd_minute_task",
+    bash_command="echo 'odd_minute_task'",
+    dag=dag,
+)
+
+follow_task = EmptyOperator(
+    task_id="follow_task",
+    trigger_rule="none_failed_or_skipped",
+    dag=dag,
+)
+
+bash_task_2 = BashOperator(
+    task_id="bash_task_2",
+    bash_command="echo 'Another bash'",
+    dag=dag,
+)
+
+# Define the task dependencies
+py_task >> bash_task >> branch_task
+branch_task >> [even_minute_task, odd_minute_task] >> follow_task >> bash_task_2
